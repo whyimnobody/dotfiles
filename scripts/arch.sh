e847957780514b5ee6bab4eb972fc16983d87d20
Kamo <kamo@sebopelo.com>
2025-05-19 17:06:08 +0200

    feat: add Arch specific script

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -1,8 +1,137 @@
-# TODO: install git, man, less, nvim, yay
-# TODO: Install stow
-# TODO: Install ghostty
-# TODO: Install yay
-# TODO: Install Brave & LibreWolf
+#!/bin/bash
+
+# Install the things we need first, because yikes
+sudo pacman -S --noconfirm --needed git less man-db
+
+# Get the dotfiles
+if [ ! -d "$HOME/.dotfiles/" ]; then
+	git clone "https://github.com/whyimnobody/dotfiles" ~/.dotfiles
+fi
+
+# Install yay
+if ! command -v yay &>/dev/null; then
+	git clone https://aur.archlinux.org/yay.git /tmp/yay && cd /tmp/yay/ && makepkg -si
+fi
+
+# Packages
+general=(
+	obsidian
+	syncthing
+)
+general_aur=(
+	1password
+	brave-bin
+	gallery-dl
+	librewolf-bin
+	wireguard-tools
+	yt-dlp-git
+	zen-browser-bin
+)
+
+media=(
+	audacity
+	gimp
+	inkscape
+	vlc
+)
+
+dev=(
+	act
+	age
+	bat
+	bottom
+	croc
+	ctop
+	dbeaver
+	diff-so-fancy
+	direnv
+	elixir
+	fd
+	fzf
+	ghostty
+	git-delta
+	git-lfs
+	glow
+	gnu-netcat
+	go
+	hugo
+	ipython
+	jq
+	just
+	lazygit
+	lsd
+	mcfly
+	neovim
+	nmap
+	nodejs
+	peco
+	pre-commit
+	ripgrep
+	rustup
+	silicon
+	source-highlight
+	starship
+	tmux
+	tree
+	uv
+	yq
+	zoxide
+)
+dev_aur=(
+	beekeeper-studio
+	lazydocker
+	lazysql
+	python-commitizen
+	rm-improved
+	tlrc
+)
+
+devops=(
+	aws-cli
+	k9s
+)
+devops_aur=(
+	flyctl
+	opentofu-bin
+)
+
+databases=(
+	postgis
+	valkey
+)
+databases_aur=(
+	pgvector
+)
+
+packages=(
+	"${general[@]}"
+	"${media[@]}"
+	"${dev[@]}"
+	"${devops[@]}"
+	"${databases[@]}"
+)
+aura=(
+	"${general_aur[@]}"
+	"${dev_aur[@]}"
+	"${devops_aur[@]}"
+	"${databases_aur[@]}"
+)
+
+sudo pacman -Syu --needed --noconfirm "${packages[@]}"
+yay -Syu --needed "${aura[@]}"
+
+# Install TPM
+if [ ! -d "$HOME/.local/share/tmux/plugins/tpm/" ]; then
+	git clone "https://github.com/tmux-plugins/tpm" ~/.local/share/tmux/plugins/tpm
+fi
+# Install tmux plugins
+"$HOME"/.local/share/tmux/plugins/tpm/scripts/install_plugins.sh
+
+# Unpack the key bits
+stow git nvim terminal tmux zsh
+
+# TODO: Sort out GPGP on system
+# TODO: Figure out a Maccy like experience
 # TODO: Install fonts and wallpapers (hyprpaper?)
 # TODO: Sort out bluetooth devices (keeb & mouse)
 # TODO: Check speakers
@@ -15,5 +144,4 @@
 # TODO: Install Steam
 # TODO: Proton
 # TODO: some games
-# TODO: Sort out nvim and and and
 # TODO: Password managers
